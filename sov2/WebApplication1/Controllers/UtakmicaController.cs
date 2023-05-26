22b3119ed46ea6ac9d16c6e09e3e841a0cc2e5a5
wildbohana <83251325+wildbohana@users.noreply.github.com>
2023-05-26 13:14:03 +0200

    сов 2

---
+++ sov2/WebApplication1/Controllers/UtakmicaController.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class UtakmicaController : Controller
+    {
+        // GET: Utakmica
+        public ActionResult Index()
+        {
+            // Vraća Utakmica/Index
+            return View();
+        }
+
+        // Add se poziva iz Index
+        [HttpPost]
+        public ActionResult Add(Utakmica utakmica)
+        {
+            // Ovo je inicijalizovano u Global.asax.cs
+            List<Utakmica> lista = (List<Utakmica>)HttpContext.Application["utakmice"];
+            
+            if (lista.Where(x => x.ID == utakmica.ID).Count() > 0)
+            {
+                Utakmica u = lista.First();
+                foreach (Utakmica u1 in lista) if (u1.ID == utakmica.ID) u = u1;
+                return View("DupliIndeks", u);
+            }
+            
+            lista.Add(utakmica);
+            return View("Tabela", lista);
+        }
+
+        // Delete se poziva iz Tabela
+        public ActionResult Delete(int id)
+        {
+            // Ovo je inicijalizovano u Global.asax.cs
+            List<Utakmica> lista = (List<Utakmica>)HttpContext.Application["utakmice"];
+            lista.Remove(lista.Where(x => x.ID == id).ElementAt(0));
+            return View("Tabela", lista);
+        }
+
+        // Filter se poziva iz Index
+        public ActionResult Filter(string Liga)
+        {
+            List<Utakmica> lista = (List<Utakmica>)HttpContext.Application["utakmice"];
+            return View("Tabela", lista.Where(x => x.Liga == Liga));
+        }
+
+        // Edit se poziva iz Tabela
+        public ActionResult Edit(int id)
+        {
+            // Ovo je inicijalizovano u Global.asax.cs
+            List<Utakmica> lista = (List<Utakmica>)HttpContext.Application["utakmice"];
+            return View("Edit", lista.Where(x => x.ID == id).ElementAt(0));
+        }
+
+        // Update se poziva iz Edit
+        [HttpPost]
+        public ActionResult Update(Utakmica u)
+        {
+            // Ovo je inicijalizovano u Global.asax.cs
+            List<Utakmica> lista = (List<Utakmica>)HttpContext.Application["utakmice"];
+            
+            foreach (var x in lista)
+            {
+                if (x.ID == u.ID)
+                {
+                    x.Gost = u.Gost;
+                    x.Domacin = u.Domacin;
+                    x.Liga = u.Liga;
+                    x.Pobednik = u.Pobednik;
+                    break;
+                }
+            }
+
+            return View("Tabela", lista);
+        }
+    }
+}
---
+++ sov2/WebApplication1/Models/Utakmica.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class Utakmica
+    {
+        public int ID { get; set; }
+        public string Gost { get; set; }
+        public string Domacin { get; set; }
+        public string Pobednik { get; set; }
+        public string Liga { get; set; }
+    }
+}
